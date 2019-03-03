f7be921f289bd3aae473ff55b010416cf77356c8
innaznayda <innaznayda@gmail.com>
2019-03-03 11:22:05 +0200

    commands to bot

--- LightBot/Controllers/HomeController.cs
+++ LightBot/Controllers/HomeController.cs
@@ -1,25 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace LightBot.Controllers {
     public class HomeController : Controller {
-        public ActionResult Index() {
-            return View();
-        }
-
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+        public string Index() {
+            return "It's my telegram bot";
         }
     }
 }
