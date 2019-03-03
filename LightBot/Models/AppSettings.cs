e8ecb17e4ec8dc027b4ac7616cb5123760cd1746
innaznayda <innaznayda@gmail.com>
2019-03-03 11:11:10 +0200

    initialise command

--- LightBot/Models/AppSettings.cs
+++ LightBot/Models/AppSettings.cs
@@ -7,8 +7,8 @@ namespace LightBot.Models {
     public static class AppSettings {
         public static string Url { get; set; }
 
-        public static string Name { get; set; }
+        public static string Name { get; set; } = "LightFirstBot";
 
-        public static string Key { get; set; }
+        public static string Key { get; set; } = "691239211:AAGeWB4EkHtSk5RWY1s_cIRM5Okly5w72dc";
     }
 }
--- LightBot/Models/Bot.cs
+++ LightBot/Models/Bot.cs
@@ -1,14 +1,25 @@
-﻿using System.Threading.Tasks;
+﻿using LightBot.Commands;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Telegram.Bot;
 
 namespace LightBot.Models {
     public static class Bot {
         private static TelegramBotClient client;
+        private static List<Command> commandsList;
+
+        public static IReadOnlyList<Command> Commands { get => commandsList.AsReadOnly(); }
 
         public static async Task<TelegramBotClient> Get() {
             if (client != null) {
                 return client;
             }
+
+            commandsList = new List<Command>();
+            commandsList.Add(new HelloCommand());
+
+            //TO DO : more commands to be initialised here
+
             client = new TelegramBotClient(AppSettings.Key);
             await client.SetWebhookAsync("");
             return client;
